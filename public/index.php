a18df5e804d0ed295877b84f38a7f86a34ebf5aa
Panos2001-10 <panagiwtiskapsochas@gmail.com>
2025-02-13 13:40:43 +0200

    -corrected a syntax error

--- public/index.php
+++ public/index.php
@@ -10,7 +10,7 @@ include __DIR__ . '/../middleware/authCheck.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Log-in/Register - Vacation Portal</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <h1>Welcome to the Vacation Portal</h1>
